5956fd5a0d9b8e33c0b065d26c22918b18602bdc
omisterdemono <47537597+omisterdemono@users.noreply.github.com>
2019-04-03 21:55:43 +0300

    Main Menu commit Zdrok

---
+++ src/main/java/MainMenu.java
@@ -0,0 +1,115 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class MainMenu {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button addUser;
+
+    @FXML
+    private Button resetUser;
+
+    @FXML
+    private Label name1;
+
+    @FXML
+    private Label price1;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Label name2;
+
+    @FXML
+    private Label price2;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Label name3;
+
+    @FXML
+    private Label price3;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Label name4;
+
+    @FXML
+    private Label price4;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Label name5;
+
+    @FXML
+    private Label price5;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Label name6;
+
+    @FXML
+    private Label price6;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    void addUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void buyClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void resetUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert addUser != null : "fx:id=\"addUser\" was not injected: check your FXML file 'Untitled'.";
+        assert resetUser != null : "fx:id=\"resetUser\" was not injected: check your FXML file 'Untitled'.";
+        assert name1 != null : "fx:id=\"name1\" was not injected: check your FXML file 'Untitled'.";
+        assert price1 != null : "fx:id=\"price1\" was not injected: check your FXML file 'Untitled'.";
+        assert button1 != null : "fx:id=\"button1\" was not injected: check your FXML file 'Untitled'.";
+        assert name2 != null : "fx:id=\"name2\" was not injected: check your FXML file 'Untitled'.";
+        assert price2 != null : "fx:id=\"price2\" was not injected: check your FXML file 'Untitled'.";
+        assert button2 != null : "fx:id=\"button2\" was not injected: check your FXML file 'Untitled'.";
+        assert name3 != null : "fx:id=\"name3\" was not injected: check your FXML file 'Untitled'.";
+        assert price3 != null : "fx:id=\"price3\" was not injected: check your FXML file 'Untitled'.";
+        assert button3 != null : "fx:id=\"button3\" was not injected: check your FXML file 'Untitled'.";
+        assert name4 != null : "fx:id=\"name4\" was not injected: check your FXML file 'Untitled'.";
+        assert price4 != null : "fx:id=\"price4\" was not injected: check your FXML file 'Untitled'.";
+        assert button4 != null : "fx:id=\"button4\" was not injected: check your FXML file 'Untitled'.";
+        assert name5 != null : "fx:id=\"name5\" was not injected: check your FXML file 'Untitled'.";
+        assert price5 != null : "fx:id=\"price5\" was not injected: check your FXML file 'Untitled'.";
+        assert button5 != null : "fx:id=\"button5\" was not injected: check your FXML file 'Untitled'.";
+        assert name6 != null : "fx:id=\"name6\" was not injected: check your FXML file 'Untitled'.";
+        assert price6 != null : "fx:id=\"price6\" was not injected: check your FXML file 'Untitled'.";
+        assert button6 != null : "fx:id=\"button6\" was not injected: check your FXML file 'Untitled'.";
+
+    }
+}
