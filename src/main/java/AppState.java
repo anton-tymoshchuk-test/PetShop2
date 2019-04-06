30a86f2be7d9fce935f753900b15c5657318f124
Lenovo <tymoshchuk.bio@gmail.com>
2019-04-06 20:54:08 +0300

    AppState

---
+++ src/main/java/AppState.java
@@ -0,0 +1,10 @@
+import javafx.stage.Stage;
+
+public abstract class AppState {
+    public static Stage logInWindow;
+    public static Stage mainMenuWindow;
+    public static Stage checkWindow;
+    public static boolean addNextWorker = false;
+    public static boolean deleteWorker = false;
+    public static boolean mainMenuShown = false;
+}
