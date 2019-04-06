9601b53f66ea496131880f428caa1ee8f1f3315d
Lenovo <tymoshchuk.bio@gmail.com>
2019-04-06 18:04:24 +0300

    Main and LogInFXML

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-public class logIn {
+public class LogIn {
 
     @FXML
     private ResourceBundle resources;
