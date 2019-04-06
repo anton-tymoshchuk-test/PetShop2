58e52a69fa593a08c09a03f0e2f6a838401f2b60
Konders <illyashykin5@gmail.com>
2019-04-06 16:57:37 +0300

    Finished database realization

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,87 +4,115 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class Database {
     public static Connection conn;
-    public static Statement statmt;
+    public static Statement statement;
     public static ResultSet resSet;
 
-    public static void Conn() throws ClassNotFoundException, SQLException {
+    public static void connect() {
         conn = null;
-        Class.forName("org.sqlite.JDBC");
-        conn = DriverManager.getConnection("jdbc:sqlite:Zoo.s3db");
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:Zoo.s3db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("База Подключена!");
     }
 
-    public static void CreateDB() throws SQLException {
-        statmt = conn.createStatement();
-
-        statmt.execute("CREATE TABLE IF NOT EXISTS `Workers` ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `Name` TEXT, `Username` TEXT, `Password` TEXT )");
-        statmt.execute("CREATE TABLE IF NOT EXISTS `Animals` ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `Type` TEXT, ` Price` REAL )");
-
+    public static void create() {
+        try {
+            statement = conn.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS `Workers` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT,`Name` TEXT,`Username` TEXT,`Password` TEXT );");
+            statement.execute("CREATE TABLE IF NOT EXISTS `Animals` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT,`Type` TEXT,`Price` REAL );");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("Таблица создана или уже существует.");
     }
 
-    public static void WriteDB() throws SQLException {
-        statmt.execute("INSERT INTO 'Animals' ('Type', 'Price') VALUES ('Monkey', 512.55); ");
-
-        System.out.println("Таблица заполнена");
-    }
-
-    public static List<Animal> getAnimals(String statement) throws SQLException {
+    public static List<Animal> getAnimals(String statement) {
         List<Animal> animals = new ArrayList<Animal>();
         String query = "SELECT * FROM Animals";
         if (statement != null)
-            query += "WHERE " + statement;
-        resSet = statmt.executeQuery(query);
-        while (resSet.next()) {
-            int id = resSet.getInt("ID");
-            String name = resSet.getString("Name");
-            double price = resSet.getDouble("Price");
-            System.out.println("ID = " + id);
-            System.out.println("Name = " + name);
-            System.out.println("Price = " + price);
-            animals.add(new Animal(id, name, price));
+            query += " WHERE " + statement;
+        try {
+            resSet = Database.statement.executeQuery(query);
+
+            while (resSet.next()) {
+                int id = resSet.getInt("ID");
+                String name = resSet.getString("Type");
+                double price = resSet.getDouble("Price");
+//                System.out.println("ID: " + id);
+//                System.out.println("Type: " + name);
+//                System.out.println("Price: " + price);
+                animals.add(new Animal(id, name, price));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return animals;
     }
 
-    public static List<Worker> getWorkers(String statement) throws SQLException {
+    public static List<Worker> getWorkers(String statement) {
         List<Worker> workers = new ArrayList<Worker>();
         String query = "SELECT * FROM Workers";
         if (statement != null)
             query += "WHERE " + statement;
-        resSet = statmt.executeQuery(query);
-        while (resSet.next()) {
-            int id = resSet.getInt("ID");
-            String name = resSet.getString("Name");
-            String username = resSet.getString("Username");
-            String password = resSet.getString("Password");
-            System.out.println("ID = " + id);
-            System.out.println("Name = " + name);
-            System.out.println("Username = " + username);
-            System.out.println("Password = " + password);
-            workers.add(new Worker(id, name, username, password));
+        try {
+            resSet = Database.statement.executeQuery(query);
+
+            while (resSet.next()) {
+                int id = resSet.getInt("ID");
+                String name = resSet.getString("Name");
+                String username = resSet.getString("Username");
+                String password = resSet.getString("Password");
+//            System.out.println("ID: " + id);
+//            System.out.println("Name: " + name);
+//            System.out.println("Username: " + username);
+//            System.out.println("Password: " + password);
+                workers.add(new Worker(id, name, username, password));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return workers;
     }
 
+    private static Map valuesTable = new HashMap() {{
+        put("Animals", "`Type`, `Price`");
+        put("Workers", "`Name`, `Username`, `Password`");
+    }};
+
     public static void insertData(String tableName, Object[] values) {
         String strValues = "";
         for (Object value : values) {
-            strValues += ", " + value;
+            if (value instanceof String)
+                strValues += ", '" + value.toString() + "'";
+            else
+                strValues += ", " + value.toString();
         }
         strValues = strValues.replaceFirst(", ", "");
-        System.out.println(strValues);
-//        statmt.execute("INSERT INTO 'Animals' ('Type', 'Price') VALUES ('Monkey', 512.55); ")
+//        System.out.println(strValues);
+        try {
+            statement.execute/*System.out.println*/(String.format("INSERT INTO `%s` (%s) VALUES (%s); ", tableName, valuesTable.get(tableName), strValues));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     //statement if null returns * else appends after statement "where"
 //    public static Object getTableData(String tableName,String statement) throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
-//        resSet = statmt.executeQuery("SELECT * FROM users");
+//        resSet = statement.executeQuery("SELECT * FROM users");
 //        List<Object> objectsData = new ArrayList<Object>();
 //
 //        while(resSet.next())
@@ -101,11 +129,15 @@ public class Database {
 //        return null;
 //    }
 
-    public static void CloseDB() throws SQLException {
-        conn.close();
-        statmt.close();
-        resSet.close();
+    public static void close() {
+        try {
+            conn.close();
 
+            statement.close();
+            resSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("Соединения закрыты");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,46 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Animal> animals = new ArrayList<Animal>();
-        ArrayList<Worker> workers = new ArrayList<Worker>();
+        List<Animal> animals = new ArrayList<Animal>();
+        List<Worker> workers = new ArrayList<Worker>();
 
-        animals.add(new Animal(0, "Cat", 127.99));
-        animals.add(new Animal(1, "Dog", 145.99));
-        animals.add(new Animal(2, "Owl", 273.99));
-        animals.add(new Animal(3, "Raccoon", 119.99));
-        animals.add(new Animal(4, "Cat", 127.99));
-        animals.add(new Animal(5, "Dog", 145.99));
-        animals.add(new Animal(6, "Owl", 273.99));
-        animals.add(new Animal(7, "Raccoon", 119.99));
-        animals.add(new Animal(8, "Cat", 127.99));
-        animals.add(new Animal(9, "Dog", 145.99));
-        animals.add(new Animal(10, "Owl", 273.99));
-        animals.add(new Animal(11, "Raccoon", 119.99));
-        animals.add(new Animal(12, "Cat", 127.99));
-        animals.add(new Animal(13, "Dog", 145.99));
-        animals.add(new Animal(14, "Owl", 273.99));
-        animals.add(new Animal(15, "Raccoon", 119.99));
+//        animals.add(new Animal(0, "Cat", 127.99));
+//        animals.add(new Animal(1, "Dog", 145.99));
+//        animals.add(new Animal(2, "Owl", 273.99));
+//        animals.add(new Animal(3, "Raccoon", 119.99));
+//        animals.add(new Animal(4, "Cat", 127.99));
+//        animals.add(new Animal(5, "Dog", 145.99));
+//        animals.add(new Animal(6, "Owl", 273.99));
+//        animals.add(new Animal(7, "Raccoon", 119.99));
+//        animals.add(new Animal(8, "Cat", 127.99));
+//        animals.add(new Animal(9, "Dog", 145.99));
+//        animals.add(new Animal(10, "Owl", 273.99));
+//        animals.add(new Animal(11, "Raccoon", 119.99));
+//        animals.add(new Animal(12, "Cat", 127.99));
+//        animals.add(new Animal(13, "Dog", 145.99));
+//        animals.add(new Animal(14, "Owl", 273.99));
+//        animals.add(new Animal(15, "Raccoon", 119.99));
 
-        workers.add(new Worker(0, "John Smith", "johnsmith", "john012"));
-        workers.add(new Worker(1, "Alex Johnson", "alexjohnson", "alex256j"));
-        workers.add(new Worker(2, "James Gordon", "james1982gordon", "g0304james"));
-        workers.add(new Worker(3, "Mary Rondes", "maryrondes23", "mr2906621rd"));
+//        workers.add(new Worker(0, "John Smith", "johnsmith", "john012"));
+//        workers.add(new Worker(1, "Alex Johnson", "alexjohnson", "alex256j"));
+//        workers.add(new Worker(2, "James Gordon", "james1982gordon", "g0304james"));
+//        workers.add(new Worker(3, "Mary Rondes", "maryrondes23", "mr2906621rd"));
+
+        Database.connect();
+        Database.create();
+
+        //As result - executes string INSERT INTO `Workers` (`Name`, `Username`, `Password`) VALUES ('John Smith', 'johnsmith', 'john012');
+        Database.insertData("Workers", new Object[]{"John Smith", "johnsmith", "john012"});
+        //As result - executes string INSERT INTO `Animals` (`Type`, `Price`) VALUES ('Cat', 127.99);
+        Database.insertData("Animals", new Object[]{"Cat", 127.99});
+        //returns all animals
+        animals = Database.getAnimals(null);
+        //or with "where" statement
+        animals = Database.getAnimals("ID = 1");
+        System.out.println(animals.get(0).getType());
+
+        Database.close();
     }
 }
